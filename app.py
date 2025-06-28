aaae2bd080d2b8c23a8e2bd05728058ca5544456
jgduron <jgduron@gmail.com>
2025-06-27 21:20:45 -0600

    Preparando app para despliegue en Render

--- app.py
+++ app.py
@@ -560,5 +560,7 @@ load_dotenv()  # Cargar variables del archivo .env
 
 PORTAL_NOMBRE = os.getenv("PORTAL_NOMBRE", "Mi Portal por Defecto")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
